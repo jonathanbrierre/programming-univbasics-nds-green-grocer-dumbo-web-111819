db67d046f2a9d3c947b17baecb32274d76036fa0
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-20 21:10:56 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -82,4 +82,9 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  consolidate_cart(cart)
+  cart
+  apply_coupons(cart,coupons)
+  cart
+  apply_clearance(cart)
 end
